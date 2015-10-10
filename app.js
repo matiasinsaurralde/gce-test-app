d9cddbc1205fe706f3b63f3f14e65f3c1c4dd22b
Matias Insaurralde <matias@insaurral.de>
2015-10-10 14:50:15 -0300

    post :p

--- app.js
+++ app.js
@@ -10,6 +10,9 @@ app.get( '/test', function( req, res ) {
   res.status(200).send( 'test' );
 });
 
+app.post( '/participar', function( req, res ) {
+});
+
 var server = app.listen( process.env.PORT || 8080, function () {
   var host = server.address().address,
       port = server.address().port;
